a22e5fa60a7c47f8002b182a30a85fef4cc28615
Fabio Carvalho <Fabio@Remote.local>
2021-03-19 01:42:26 +0530

    added template for index

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 #All routes will be listed here
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
-
+    user = {"username":"Fabio"}
+    title = "Home"
+    return render_template("index.html", title=title, user=user)
